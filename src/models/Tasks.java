8c394318c17a5c22ff6344bfc71be5c98d304385
ayaka.isozaki <ayaka.isozaki@gmail.com>
2023-05-21 16:48:08 +0900

    ページネーションを追加

--- src/models/Tasks.java
+++ src/models/Tasks.java
@@ -7,10 +7,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
-
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
+import javax.persistence.Table;
 
 @Entity
 @NamedQueries({
@@ -19,6 +18,10 @@ import javax.persistence.NamedQuery;
         query = "SELECT m FROM Tasks AS m ORDER BY m.id DESC"
     )
 })
+@NamedQuery(
+        name = "getTasksCount",
+        query = "SELECT COUNT(m) FROM Tasks AS m"
+        )
 @Table(name = "tasks")
 public class Tasks {
     @Id
